601b922804155d585e9ad14a34eebaf539089097
Sarah Whalley <s.whalley1130@gmail.com>
2023-03-17 14:37:22 -0600

    Completes lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES 
+("Mr. Chocolate",5,"M", "brown", "chill", 1),
+("Rowdy",6,"M", "blue", "rowdy", 0),
+("Tabitha",10,"F", "green", "tame", 1),
+("Sergeant Brown",1,"F", "black", "militant", 1),
+("Melissa",80,"F", "purple", "just vibing", 0),
+("Grinch",6,"M", "cream", "evil", 0),
+("Wendy",5,"F", "orange", "more evil", 1),
+(NULL,0,"F", "orange", "possibly Wendy's child", 1);
+
